d9942d5268a6426bda5539c119a1407d79f81fad
KohGeek <KohComputer@gmail.com>
2021-09-07 03:36:40 +0800

    Fix colour not loaded properly after syncing

--- src/components/functions.js
+++ src/components/functions.js
@@ -97,7 +97,7 @@ export async function syncToServer(operation) {
                 item.id,
                 item.name,
                 item.priority,
-                item.color,
+                item.colour,
                 item.reminder,
                 item.completed,
               ],
